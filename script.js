b8394ee920e5369b6046d6cc13f2834a7bf860db
nathanmkc <nkellehe8@gmail.com>
2021-03-03 01:21:17 -0800

    prepping for ec2 launch

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ export let options = {
   scenarios: {
     constant_request_rate: {
       executor: 'constant-arrival-rate',
-      rate: 500,
+      rate: 1000,
       timeUnit: '1s', // iterations per second, i.e. 1000 RPS
       duration: '60s', // duration the iterations will attempt to run for
       preAllocatedVUs: 100, // how large the initial pool of VUs would be (match the rps)
-      maxVUs: 700, // if the preAllocatedVUs are not enough, we can initialize more,
+      maxVUs: 500, // if the preAllocatedVUs are not enough, we can initialize more,
     },
   },
 };
 
 export default function kSix () {
   http.get('http://localhost:3001/shoes/9938485/colors/1/quantities');
-  sleep(1);
+  //sleep(1);
 }
